e59a00c362e4525c8e2520ae7e3546e9df3a593e
Olya Khromova <olya.hrmvlg@gmail.com>
2024-12-12 15:05:08 +0300

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-Вы можете посетить сайт по ссылке: https://hrmvlg.github.io/positivus/ <br>
-You can visit the website by clicking here: https://hrmvlg.github.io/positivus/
--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-    base: '/positivus/',
-    build: {
-        outDir: 'build',
-        emptyOutDir: true,
-    },
-});
